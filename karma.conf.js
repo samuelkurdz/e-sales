1e01adde5298020988bbb512a070cc7de79b8c7b
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-06-16 13:59:01 +0100

    linting

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tailwindproject'),
+      dir: require('path').join(__dirname, './coverage/nuella'),
       subdir: '.',
       reporters: [
         { type: 'html' },
